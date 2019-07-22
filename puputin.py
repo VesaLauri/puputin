08221797eb5ff5178167ca17c449ae462b5bf046
VesaLauri <vvlauri@gmail.com>
2019-07-22 13:16:35 +0300

    huhuu

--- puputin.py
+++ puputin.py
@@ -16,7 +16,7 @@ class Puputin:
     def nimi(self, nimi):
         self._nimi = nimi
 
-    def uusipuppu(self, puppu, **kwargs):
+    def uusipuppu(self, puppu, *args, **kwargs):
         if not isinstance(puppu, pd.DataFrame):
             raise TypeError("Syön Vain pndas dataframeja")
 
@@ -26,12 +26,19 @@ class Puputin:
             puppu["kumulatiivinensumma"] = data[int(kwargs["frekvenssi"])].cumsum()
         else:
             data["kumulatiivinensumma"] = data.index.values.astype(int)
+
+        tulostetaan = list(args)
+
+        puppu.__setattr__("tulostetaan", args)
         self._puput.append(puppu)
 
     def annapuppu(self, monta: int = 1):
+        # for i in range(1):
+        print(datasetti.__getattr__("tulostetaan"))
+        print(self._puput[0].__getattr__("tulostetaan"))
         datasetti = self._puput[0]
         arpa = random.randint(0, self._puput[0]["kumulatiivinensumma"].iloc[-1])
-        return datasetti[datasetti["kumulatiivinensumma"] >= arpa].iloc[0:1]
+        return datasetti[datasetti["kumulatiivinensumma"] >= arpa].iloc[0:1, [0, 1, 2]]
 
 
 if __name__ == "__main__":
@@ -40,8 +47,8 @@ if __name__ == "__main__":
     data = pd.read_csv(
         "tekstit\\miesnimet.txt", sep=",", header=None, encoding="ISO-8859-1"
     )
-    puppu.uusipuppu(data)
+    puppu.uusipuppu(data, [0, 1, 2])
     print(puppu.annapuppu())
-    puppu2.uusipuppu(data, frekvenssi=1)
+    puppu2.uusipuppu(data, [0, 1, 2], frekvenssi=1)
     print(puppu2.annapuppu())
 
